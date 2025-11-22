bc7c7dde25efc84c839ccf40c7bc82eedc4252ca
hamed-rezaee <hamed-rezaee@users.noreply.github.com>
2025-11-22 12:08:37 +0000

    deploy: eabe444dc395625f6e7e622828185f8e0ca363fb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "700059446"
+    serviceWorkerVersion: "3053352405"
   }
 });
